fc24e1d79af9c4cc12cfa3ad61fded8c66aee2b1
Pakhomov Alexandr <WorkAlexAn@gmail.com>
2022-01-27 15:11:47 +0300

    Initial commit

---
+++ app/src/main/java/com/alexpaxom/gitsearch/ui/features/mainwindow/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alexpaxom.gitsearch.ui.features.mainwindow.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alexpaxom.gitsearch.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
